75d6739b740d3a80e2b55bf9d0a8559aaf969aeb
Soheil <soheilzarepoor79@gmail.com>
2024-09-20 15:21:42 +0330

    feat: play song preview on playlist page

--- components/playlists/song/item.vue
+++ components/playlists/song/item.vue
@@ -2,11 +2,18 @@
     <div class="w-full px-2 py-5 flex justify-between items-center">
         <div class="flex gap-4 items-center">
             <span>{{ listPosition }}</span>
-            <img
-                :src="song.cover"
-                :alt="song.title"
-                class="w-16 h-16 object-cover rounded-md"
-            />
+            <div class="w-16 h-16 relative flex justify-center group">
+                <img
+                    :src="song.cover"
+                    :alt="song.title"
+                    class="object-cover aspect-square w-full rounded-md"
+                />
+                <PlaylistsSearchSongPlayBtn
+                    class="group-hover:opacity-100"
+                    :src="song.preview"
+                    @play="(src) => setCurrentSongSrc(src)"
+                />
+            </div>
             <div class="flex flex-col justify-center">
                 <span class="text-white line-clamp-1">{{ song.title }}</span>
                 <span class="text-gray-400 line-clamp-1">{{
@@ -26,4 +33,16 @@ interface Props {
 }
 
 defineProps<Props>();
+
+const currentSongSrc = usePlayingSong();
+
+const setCurrentSongSrc = (src: string) => {
+    if (!isCurrentIsPlaying(src)) {
+        currentSongSrc.value = src;
+    } else {
+        currentSongSrc.value = "";
+    }
+};
+
+const isCurrentIsPlaying = (src: string) => src === currentSongSrc.value;
 </script>
